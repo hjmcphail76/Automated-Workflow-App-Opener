f87b36a5b799b97f83e156310dcd13608194abd1
hjmcphail76 <hjmcphail@gmail.com>
2024-06-29 13:09:17 -0400

    Added the menu with 2 simple workflows.

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 from AppOpener import open
 import customtkinter
 
-application_list = ["spotify", "advantagescope wpilib"]
-#open("advantagescope wpilib")
+
+def open_programming_Workflow():
+    open("wpilib advantagescope",match_closest=True)
+    open("prusaslicer")
+    open("google chrome")
+    open("spotify")
+    app.destroy()
+
+def open_3d_printing_Workflow():
+    open("prusaslicer")
+    open("google chrome")
+    open("spotify")
+    app.destroy()
+
+function_lst = [open_programming_Workflow, open_3d_printing_Workflow]
+button_names_lst=["Programming Workflow", "3D Printing Workflow"]
+button_lst = []
+
+
 
 customtkinter.set_appearance_mode("Dark")
-customtkinter.set_default_color_theme("blue") 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+customtkinter.set_default_color_theme("green")
+app = customtkinter.CTk()
 app.geometry("800x580")
-lst=[]
-for i in range(5):
-    lst.append(customtkinter.CTkButton(master=app, text="CTkButton", command=None,corner_radius=20).grid(row=0, column=i))
+
+
+for i in range(len(button_names_lst)):
+    button_lst.append(
+        customtkinter.CTkButton(
+                                master=app, 
+                                text=button_names_lst[i],
+                                command=function_lst[i],corner_radius=20,
+                                border_color="black",
+                                border_width=3
+                                )
+                                    .pack(side="left", 
+                                        fill="both", 
+                                        expand=True))
 
 
 app.mainloop()
